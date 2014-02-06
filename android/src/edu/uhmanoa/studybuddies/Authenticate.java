92d852019c86f53dcf6b7091620353be0d1aa3bd
Erika Nana <enana@hawaii.edu>
2014-02-05 20:33:57 -1000

    Connection timed out when accessing root page

--- android/src/edu/uhmanoa/studybuddies/Authenticate.java
+++ android/src/edu/uhmanoa/studybuddies/Authenticate.java
@@ -1,7 +1,10 @@
 package edu.uhmanoa.studybuddies;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
 
 import org.jsoup.Connection;
 import org.jsoup.Connection.Method;
@@ -26,6 +29,10 @@ import android.widget.EditText;
 
 //first you need to get the page so you can get the uuid and post on the same instance
 
+//try using BANSSO and badprotocol cookies, probably not, since tamper data didn't affect it
+//try setting the headers --> uPortal-version header
+//nope never mind --> figure out what is the difference between a response header and a request header
+//try changing the user agent, logging in on wifi, it probably has to do with the headers
 public class Authenticate extends Activity implements OnClickListener {
 	
 	Button mLoginButton;
@@ -38,10 +45,33 @@ public class Authenticate extends Activity implements OnClickListener {
 	String mPassword;
 	String mCookieValue;
 	
+	//urls
 	public static final String POST_LOGIN_URL = "https://myuh.hawaii.edu/cp/home/login";
-	public static final String COOKIE_TYPE = "JSESSIONID";
+	public static final String HOME_LOGIN = "https://myuh.hawaii.edu/cp/home/displaylogin";
+	public static final String OK_URL = "http://myuh.hawaii.edu/render.userLayoutRootNode.uP";
+	
+	//Request header stuff
+	public static final String REFERRER = "http://myuh.hawaii.edu/cps/welcome/loginok.html";
+	public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0";
+	public static final String ACCEPT_HEADER = "Accept";
+	public static final String ACCEPT_HEADER_VALUE = "	text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+	public static final String ACCEPT_ENCODING = "Accept-Encoding";
+	public static final String ACCEPT_ENCODING_HEADER_VALUE = "gzip, deflate";
+	public static final String ACCEPT_LANGUAGE_HEADER = "Accept-Language";
+	public static final String ACCEPT_LANGUAGE_HEADER_VALUE = "en-US,en;q=0.5";
+	public static final String CONNECTION_HEADER = "Connection";
+	public static final String CONNECTION_HEADER_VALUE = "keep-alive";
+	public static final String HOST_HEADER = "Host";
+	public static final String HOST_HEADER_VALUE = "myuh.hawaii.edu";
+	
+	//error codes
 	public static final int WRONG_INPUT_ERROR = 1;
 	public static final int CONNECTION_ERROR = 2;
+	
+	Map<String, String> loginCookies = null;
+	
+	/**Values for data passed into the intent*/
+	public static final String COOKIES = "Cookies";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -67,9 +97,7 @@ public class Authenticate extends Activity implements OnClickListener {
 		if (button.getId() == R.id.loginButton) {
 
 /*			mUserName = mUserNameBox.getText().toString();
-			mPassword = mPasswordBox.getText().toString();
-			Log.w("name", mUserName);
-			Log.w("password", mPassword);*/
+			mPassword = mPasswordBox.getText().toString();*/
 			mUserName = "enana";
 			mPassword = "fUcktbs!1!";
 			login();
@@ -86,9 +114,10 @@ public class Authenticate extends Activity implements OnClickListener {
 			Document getDoc = null;
 			String getResponse = "";
 			String uuid = "";
+			
 			try {
 				//get the page response first
-				Connection.Response get = Jsoup.connect("https://myuh.hawaii.edu/cp/home/displaylogin")
+				Connection.Response get = Jsoup.connect(urls[0])
 						.method(Method.GET)
 						.execute();
 				
@@ -111,19 +140,42 @@ public class Authenticate extends Activity implements OnClickListener {
 				}
 				scanner.close();
 
-				Connection.Response res = Jsoup.connect(urls[0])
+				Connection.Response res = Jsoup.connect(urls[1])
 						.data("user", mUserName)
 						.data("pass", mPassword)
 						.data("uuid",uuid)
 						.method(Method.POST)
 						.execute();
-				
+
 				doc = res.parse();
+				loginCookies = res.cookies();
 				mLoginResponse = doc.toString();
+
+				//get the cookies
+/*				mCookieValue = res.cookie(COOKIE_TYPE);*/
+/*				Log.w("cookie", mCookieValue);*/
 				
-				//get the cookie
-				mCookieValue = res.cookie(COOKIE_TYPE);
-				Log.w("cookie", mCookieValue);
+				//do more connecting?
+				Connection.Response follow = Jsoup.connect(urls[2])
+						.method(Method.GET)
+						.cookies(loginCookies)
+						.referrer(REFERRER)
+						.userAgent(USER_AGENT)
+						.header(ACCEPT_HEADER, ACCEPT_HEADER_VALUE)
+						.header(ACCEPT_ENCODING, ACCEPT_ENCODING_HEADER_VALUE)
+						.header(ACCEPT_LANGUAGE_HEADER,ACCEPT_LANGUAGE_HEADER_VALUE)
+						.header(CONNECTION_HEADER, CONNECTION_HEADER_VALUE)
+						.header(HOST_HEADER, HOST_HEADER_VALUE)
+						.followRedirects(true)
+						.ignoreHttpErrors(true)
+						.data("uP_root", "root")
+						.timeout(5000)
+						.execute();
+				
+				getDoc = follow.parse();
+				getResponse = getDoc.toString();
+				Log.w("response",getResponse);
+				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -132,34 +184,35 @@ public class Authenticate extends Activity implements OnClickListener {
 		}
         @Override
         protected void onPostExecute(String response) {
-                if (response != null) {
-/*                	Log.w("response", response);    */          		
-                	if (response.contains("Successful")) {
-	                        pd.dismiss();
-	                        Log.w("authenticate","YAY!!!");
-                		}
-                		else {
-                			showErrorDialog(WRONG_INPUT_ERROR);
-                		}
-                }
-                else {
-                	showErrorDialog(CONNECTION_ERROR);
-                }
-/*                if (response != null) {
-                        if (response.contains("Welcome")) {
-                                launchMainStudentMenu();
-                        }
-                        else {
-                                showErrorDialog(WRONG_INPUT_ERROR);
-                        }
-                }
-                else { //wasn't able to connect at all
-                        showErrorDialog(CONNECTION_ERROR);
-                }
-        }*/
+	        if (response != null) {          		
+	        	if (response.contains("Successful")) {
+	                    pd.dismiss();
+	                    Log.w("authenticate","YAY!!!");
+	                    
+	                    launchGetClasses();
+	        		}
+	        		else {
+	        			showErrorDialog(WRONG_INPUT_ERROR);
+	        		}
+	        }
+	        else {
+	        	showErrorDialog(CONNECTION_ERROR);
+	        }
         }
     }
 	
+	public void launchGetClasses() {
+        Intent launchGetClasses = new Intent(this,GetClasses.class);
+		Set<Entry<String, String>> entries = loginCookies.entrySet();
+		
+		//store the cookies
+		Bundle cookies = new Bundle();
+		for (Entry<String, String> entry : entries) {
+			cookies.putString(entry.getKey(), entry.getValue());
+		}
+		launchGetClasses.putExtra(COOKIES, cookies);
+		startActivity(launchGetClasses);
+	}
 	public void login() {
 		//this requires min APK of 14 or higher
 		pd = new ProgressDialog(this, ProgressDialog.THEME_DEVICE_DEFAULT_DARK);
@@ -168,7 +221,7 @@ public class Authenticate extends Activity implements OnClickListener {
 		pd.setIndeterminate(true);
 		pd.show();
 		connectToWebsite connect = new connectToWebsite();
-		connect.execute(new String [] {POST_LOGIN_URL, mUserName, mPassword});
+		connect.execute(new String [] {HOME_LOGIN, POST_LOGIN_URL, OK_URL, mUserName, mPassword});
 	}
 	
 	public void showErrorDialog(int typeOfError) {
