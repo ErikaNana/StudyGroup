d901dbcb9c5c10cf4491056b7a14df2d5031cfc3
Erika Nana <enana@hawaii.edu>
2014-04-05 16:04:59 -1000

    Added initial controller, view and route for posting json from android app #3

--- study_group/app/controllers/static_controller.rb
+++ study_group/app/controllers/static_controller.rb
@@ -2,4 +2,12 @@ class StaticController < ApplicationController
 
     def welcome
     end
+
+    def create
+    end
+
+    def display_json
+        #for now
+        render 'create'
+    end
 end
--- study_group/config/routes.rb
+++ study_group/config/routes.rb
@@ -1,3 +1,5 @@
 StudyGroup::Application.routes.draw do
   root :to => "static#welcome"
+  match 'create', to: 'static#create',     via: 'post'
+  match 'create', to: 'static#display_json',     via: 'get'
 end
