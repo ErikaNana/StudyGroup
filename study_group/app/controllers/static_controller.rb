62deee5698abafc2d5ff92cfd35d75516fb751d3
ErikaNana <enana@hawaii.edu>
2014-04-27 21:42:33 -0700

    Created Course model and create course in controller

--- study_group/app/controllers/static_controller.rb
+++ study_group/app/controllers/static_controller.rb
@@ -16,9 +16,35 @@ class StaticController < ApplicationController
     end
 
     def display_schedule
-    schedule = params[:userSchedule]
-    user = schedule[:user]
-    # [{name: name, courseInfo: [{day: days, time: times}, {day: days}, time: times]}]
-    courses = schedule[:courses]
+        schedule = params[:userSchedule]
+        @user = schedule[:user]
+
+        # [{name: name, courseInfo: [{day: days, time: times}, {day: days}, time: times]}]
+        courses = schedule[:courses]
+
+        #create course objects
+        @courseArray = Array.new
+
+        #create arrays
+        courses.each do |course|
+            days = ""
+            times = ""
+            day_and_times = course[:courseInfo] #array of hashmaps
+
+            #build strings
+            day_and_times.each do |info|
+                days = days + info[:day] + ","
+                times = times + info[:time] + ","
+            end
+            #remove last commas
+            days.chop!
+            times.chop!
+
+            #create course objects
+            #need to check if it exists first, but this can be done later
+            add_course = Course.create(name: course[:name], days:days, times:times)
+            #if it exists, just add that
+            @courseArray.push(add_course)
+        end
     end
 end
--- study_group/db/schema.rb
+++ study_group/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140427120634) do
+ActiveRecord::Schema.define(version: 20140428040705) do
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.string   "days"
+    t.string   "times"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "students", force: true do |t|
     t.string   "email"
